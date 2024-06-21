4ec1681eeee5c65a18607467b36420482c7ec0d4
xiaojiangxj233 <xiaojiang233@outlook.com>
2024-06-21 19:39:20 +0800

    log

--- scripts/main.py
+++ scripts/main.py
@@ -2,8 +2,12 @@ import requests
 import datetime
 import os
 
+image_path: str = "null"
+enable_logs: bool = bool(os.getenv("ENABLE_ERROR_LOGS"))
+
 
 def try_get():
+    global image_path
     # 请求API获取数据
     response = requests.get('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN')
     data = response.json()
@@ -25,19 +29,32 @@ def try_get():
         file.write(image_response.content)
 
     print(f"Image saved to {image_path}")
+    # raise Exception("wtf")
 
 
 def main():
     count = 0
+    global enable_logs
     while True:
         try:
             try_get()
-            break
+
+
         except Exception as e:
+
             print(f"[{count}]Error: {e} - Failed to download image, retrying...")
             count += 1
-
-    print(f"Downloaded image in {count} attempt(s).")
+            if count >= 10 and enable_logs:
+                log_path = f"./images/error-{datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')}.txt"
+                with open(log_path, 'w') as file:
+                    file.write(f"Error when downloading image: {str(e)}")
+                print(f"[{count}] Error: {e} - Failed to download image, saved error log as {log_path}")
+                break
+            elif count >= 10:
+                print(f"[{count}] Error: {e} - Failed to download image,exiting...")
+        else:
+            print(f"Downloaded image in {count} attempt(s).")
+            break
 
 
 if __name__ == "__main__":
